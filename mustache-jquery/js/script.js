4abdc6bedddb2d426edcc0259d7463988f31f4e5
Russell Matney <russell.matney@gmail.com>
2013-11-23 14:24:19 -0500

    adds basic example using external template

--- mustache-jquery/js/script.js
+++ mustache-jquery/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-  var data = {
+
+  // creates json object
+  var jsonData = {
     name: {
       first: "sir",
       last: "russell"
@@ -8,8 +10,22 @@ $(document).ready(function(){
     favorite_color: "blue"
   }
 
-  var rendered = $.mustache("{{name.last}}, {{name.first}}: {{quest}}", data);
-  
-  $("#person").html(rendered);
+  // hands json and template into $.mustache function
+  var renderedHtml = $.mustache("{{name.last}}, {{name.first}}: {{quest}}", jsonData);
+
+  // replaces person id with rendered template via jquery's .html() function 
+  $("#person").html(renderedHtml);
+
+
+  // this works, but not statically – you'll need to run a simple server for the Access-Control-Allow-Origin errors to go away
+  $.get('./partials/person.html', function(template) {
+    
+    // or, another way
+    var html = Mustache.to_html(template, jsonData);
+
+    $("#anotherPerson").html(html);
+  });
+
+
 
 });
